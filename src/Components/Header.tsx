82477188b4af2912cb1718499d119335c93ba2cd
Greisvander <pestryakov_89@mail.ru>
2020-12-26 01:32:59 +0300

    Lesson 12, Breakdown into components

---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Header = () => {
+    return (
+
+            <header className={'Header'}>
+                <img src="https://w-dog.ru/wallpapers/5/9/288879431917547/polyarnaya-sova-belaya-sova-ptica.jpg"
+                     alt="image"/>
+            </header>
+
+    )
+}
+export default Header
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className={'Content'}>
+            <div><img src="https://img3.goodfon.ru/original/1920x1200/4/dd/abstract-colors-unreal-clouds-1927.jpg"
+                      alt="fon"/></div>
+            <div className={'Ava'}><img
+                src="https://photopict.ru/wp-content/uploads/2019/05/prikol-nye-kartinki-na-avu1.jpg"
+                alt="Ava"/>
+                Ava +Description
+            </div>
+            <div>Myposts
+                <div>New Post</div>
+                <div>post1</div>
+                <div>post2</div>
+                <div>post3</div>
+                <div>post4</div>
+            </div>
+
+
+        </div>
+ 
+}
+export default Profile
