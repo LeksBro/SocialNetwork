7978dbf14f66c1601cfdcaf8ffc1ade26e8f4e74
Greisvander <pestryakov_89@mail.ru>
2020-12-27 02:45:59 +0300

    Lesson 21, typesetting the Dialogs component

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import s from './Dialogs.module.css'
 const Dialogs = () => {
-    return <div>
-        <input type="text"/>
+    return <div className={s.dialogs}>
+        <div className={s.dialogItem}>
+            <div className={s.dialog}>Sasha</div>
+            <div className={s.dialog}>Meda</div>
+            <div className={s.dialog}>Victor</div>
+            <div className={s.dialog}>Alena</div>
+            <div className={s.dialog}>Dasha</div>
+        </div>
+        <div className={s.messageItem}>
+            <div className={s.message}>hi</div>
+            <div className={s.message}>How are</div>
+            <div className={s.message}>do you want a car</div>
+        </div>
     </div>
 }
 export default Dialogs
