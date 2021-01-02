81b550db41efd23d1babd63fd9926b54a7bc30fe
Greisvander <pestryakov_89@mail.ru>
2021-01-02 15:22:18 +0300

    Lesson 24, take the data aside

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -2,8 +2,29 @@ import React from "react";
 import s from './Dialogs.module.css'
 import Dialog from "./Dialog/Dialog";
 import Message from "./Message/Message"
-
+type DialogType = {
+    id:number
+    name: string
+}
+type MessageType = {
+    id:number
+    message: string
+}
 const Dialogs = () => {
+    let dialogData: Array<DialogType> = [
+        {id: 1, name: 'Sasha'},
+        {id: 2, name: 'Meda'},
+        {id: 3, name: 'Victor'},
+        {id: 4, name: 'Alena'},
+        {id: 5, name: 'Dasha'},
+    ]
+    let messageData: Array<MessageType> = [
+        {id: 1, message:'i understand'},
+        {id: 2, message:'i understand you'},
+        {id: 3, message:'i see you'},
+    ]
+
+
     return <div className={s.dialogs}>
         <div className={s.dialogItem}>
             < Dialog id={1} name={'Sasha'} />
--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Posts from "./Post/Posts";
+type PostType = {
+    message: string
+    likeCount: number
+    id: number
+}
 
 const MyPosts = () => {
+    let postData: Array<PostType> = [
+        {message: 'How are you', likeCount: 2, id: 1},
+        {message: 'hi', likeCount: 0, id: 2},
+        {message: 'good morning', likeCount: 22, id: 3},
+    ]
     return <div className={s.content}>
         <div className={s.block}>
             <div>
