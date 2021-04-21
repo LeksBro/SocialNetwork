b89edab01bc4169d4c7a60c8cdd5b55ddb3e09f5
Greisvander <pestryakov_89@mail.ru>
2021-04-21 21:02:30 +0300

    Lesson 85 hook useEffect

--- src/Components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/Components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,29 +1,33 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import {ProfileStatusType} from "./ProfileStatus";
 
 const ProfileStatusWithHooks = (props: ProfileStatusType) => {
 
-  let [editMode, setEditMode] = useState(false)
+    let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState<string>(props.status)
 
-  const activateMode = () => {
-      setEditMode(true)
-  }
-  const deactivateEditMod = () => {
-      setEditMode(false)
-      props.updateStatus(status)
+    useEffect(() => {
+        setStatus(props.status)
+    },[props.status])
+
+    const activateMode = () => {
+        setEditMode(true)
     }
-  const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
-       setStatus(e.currentTarget.value)
-  }
+    const deactivateEditMod = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    }
 
     return (
         <div>
-        { !editMode &&
-        <div>
-            <span onDoubleClick={activateMode}>{props.status || '----'}</span>
-        </div>
-        }
+            {!editMode &&
+            <div>
+                <span onDoubleClick={activateMode}>{props.status || '----'}</span>
+            </div>
+            }
             {editMode &&
             <div>
                 <input autoFocus value={status} onBlur={deactivateEditMod} onChange={onStatusChange}/>
